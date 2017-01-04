599396fd3bc64a6747aeeb4dac577bf81e39813e
sasaplus1 <sasaplus1@gmail.com>
2017-01-04 21:40:19 +0900

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const path = require('path');
+
+const webpack = require('webpack');
+
+const {
+  name,
+  repository,
+  version,
+} = require('./package');
+
+module.exports = {
+
+  context: __dirname,
+
+  target: 'web',
+
+  entry: './index.js',
+
+  output: {
+    path: __dirname,
+    filename: '[name].js',
+    library: [
+      name.replace(/-[a-z]/g,
+        (s) => s.slice(1).toUpperCase()
+      ),
+    ],
+    libraryTarget: 'umd',
+  },
+
+  resolveLoader: {
+    root: path.join(__dirname, 'node_modules'),
+  },
+
+  node: {
+    Buffer: false,
+    process: false,
+  },
+
+  resolve: {
+    extensions: [
+      '',
+      '.js',
+    ],
+    modulesDirectories: [
+      'node_modules',
+    ],
+  },
+
+  plugins: [
+    new webpack.NoErrorsPlugin,
+    new webpack.IgnorePlugin(/vertx/),
+    new webpack.optimize.OccurenceOrderPlugin,
+    new webpack.optimize.DedupePlugin,
+    new webpack.optimize.AggressiveMergingPlugin,
+    new webpack.BannerPlugin([
+      `@license ${name}.js ver.${version} Copyright(c) 2017 sasa+1`,
+      repository.url.replace(/\.git$/i, ''),
+      'Released under the MIT license.',
+    ].join('\n'), {
+      options: {
+        raw: false,
+        entryOnly: true,
+      },
+    })
+  ],
+
+};
